5e7d0c6cf47acb343f8584dca9fa0244427b0cc8
sorinxv <sorinmartinescu06@gmail.com>
2022-10-25 17:10:23 +0200

    navbar update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,15 @@
 import Link from 'next/link'
 export default function Navbar() {
     return (
-      <>
-          <ul>
-              <li> <Link href="/"><a>Home</a></Link> </li>
-              <li> <Link href="/cart"><a>Cart</a></Link> </li>
-              <li> <Link href="/favorite"><a>Favorite</a></Link> </li>
-              <li> <Link href="/profile"><a>Profile</a></Link> </li>
-              <li> <Link href="/help"><a>Help</a></Link> </li>
+      <div className='bg-teal-700 flex justify-between p-4 pr-20 pl-20'>
+        <li className='m-2 font-extrabold'>Market<span className='text-emerald-300 font-black'>Place</span></li>
+          <ul className='flex font-bold'>
+              <li className='m-2'> <Link href="/"><a>Home</a></Link> </li>
+              <li className='m-2'> <Link href="/explore"><a>Explore</a></Link> </li>
+              <li className='m-2'> <Link href="/saved"><a>Saved</a></Link> </li>
+              <li className='m-2'> <Link href="/help"><a>Help</a></Link> </li>
           </ul>
-      </>
+        </div>
     )
   }
   
