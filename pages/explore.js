df9b9e29769d5a8147fd2ea94860810d88ad0278
sorinxv <sorinmartinescu06@gmail.com>
2022-10-25 17:09:22 +0200

    changed name of pages + updated their path

--- pages/explore.js
+++ pages/explore.js
@@ -2,6 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function cart() {
-  return <>cart page</>
+export default function explore() {
+  return <>explore page</>
 }
--- pages/saved.js
+++ pages/saved.js
@@ -2,6 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function cart() {
-  return <>favorite page</>
+export default function saved() {
+  return <>saved page</>
 }
