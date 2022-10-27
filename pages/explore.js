62b0ed796398ea8603d8f1e816f8b0a5aebf1269
sorinxv <sorinmartinescu06@gmail.com>
2022-10-27 09:15:44 +0200

    moved cards to explore page

--- pages/explore.js
+++ pages/explore.js
@@ -1,7 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import CardsLayout from '../components/CardsLayout'
 
-export default function explore() {
-  return <>explore page</>
+export default function explore({ cardDetails }) {
+  return (
+    <>
+      <CardsLayout details={cardDetails} />
+    </>
+  )
+}
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/cardIndex')
+  const data = await res.json()
+
+  return { props: { cardDetails: data } }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import CardsLayout from "../components/CardsLayout";
-export default function Home({ cardDetails }) {
+export default function Home() {
   return <>
-    <CardsLayout details={cardDetails} />
   </>;
 }
 
-export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/cardIndex')
-  const data = await res.json();
-
-  return { props: { cardDetails: data } }
-}
 
