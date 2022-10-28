b2b821ad5ab9cdea806501270a0ad7e9c19be7b8
sorinxv <sorinmartinescu06@gmail.com>
2022-10-28 16:02:40 +0200

    navbar + (explore page color fixed)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,94 @@
+import { useState } from 'react'
 import Link from 'next/link'
-export default function Navbar() {
-    return (
-      <div className='bg-teal-700 flex justify-between p-4 pr-20 pl-20'>
-        <li className='m-2 font-extrabold'>Market<span className='text-emerald-300 font-black'>Place</span></li>
-          <ul className='flex font-bold'>
-              <li className='m-2'> <Link href="/"><a>Home</a></Link> </li>
-              <li className='m-2'> <Link href="/explore"><a>Explore</a></Link> </li>
-              <li className='m-2'> <Link href="/saved"><a>Saved</a></Link> </li>
-              <li className='m-2'> <Link href="/help"><a>Help</a></Link> </li>
-          </ul>
+export default function NavBar() {
+  const [navbar, setNavbar] = useState(false)
+
+  return (
+    <nav className="w-full bg-purple-500 shadow">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="javascript:void(0)">
+              <li className="m-2 font-extrabold text-white">
+                Market<span className="green-color font-black">Place</span>
+              </li>
+            </a>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-white"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
         </div>
-    )
-  }
-  
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? 'block' : 'hidden'
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-white hover:green-color">
+                <Link href="/">
+                  <a className='font-bold'>Home</a>
+                </Link>
+              </li>
+              <li className="text-white hover:green-color">
+                <Link href="/explore">
+                  <a className='font-bold'>Explore</a>
+                </Link>
+              </li>
+              <li className="text-white hover:green-color">
+                <Link href="/saved">
+                  <a className='font-bold'>Saved</a>
+                </Link>
+              </li>
+              <li className="text-white hover:green-color">
+                <Link href="/help">
+                  <a className='font-bold'>Help</a>
+                </Link>
+              </li>
+            </ul>
+
+            <div className="mt-3 space-y-2 md:hidden md:inline-block">
+              
+            </div>
+          </div>
+        </div>
+        <div className="hidden space-x-2 md:inline-block">
+         
+        </div>
+      </div>
+    </nav>
+  )
+}
