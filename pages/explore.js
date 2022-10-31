90c65cb99b42fc8693c5222e535cf32c5511a997
sorinxv <sorinmartinescu06@gmail.com>
2022-10-31 13:32:27 +0100

    added page description placeholder

--- pages/explore.js
+++ pages/explore.js
@@ -1,12 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import CardsLayout from '../components/CardsLayout'
 
 export default function explore({ cardDetails }) {
   return (
     <>
+    <center>
+      <h1 className='title mt-12'>Explore</h1>
+      <p className='mb-12'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+      <p>we'll also have the sort thing somewhere, hopefully</p>
+    </center>
       <CardsLayout details={cardDetails} />
+      
     </>
   )
 }
