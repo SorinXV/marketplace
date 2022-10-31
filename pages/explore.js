7f78b8d5df3f8b53128c37736691980cb72042fc
sorinxv <sorinmartinescu06@gmail.com>
2022-10-31 13:35:04 +0100

    fixing failing check

--- pages/explore.js
+++ pages/explore.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+
+import styles from '../styles/Home.module.css'
 import CardsLayout from '../components/CardsLayout'
 
 export default function explore({ cardDetails }) {
