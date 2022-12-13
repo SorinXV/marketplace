be04356079f14c16709d6629e3f0562cf8ef1302
sorinxv <sorinmartinescu06@gmail.com>
2022-12-13 14:39:08 +0100

    margin

--- pages/explore.js
+++ pages/explore.js
@@ -1,15 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
 import styles from '../styles/Home.module.css'
 import CardsLayout from '../components/CardsLayout'
 
 
-
 export default  function explore() {
   return (
-    <>          
-             <CardsLayout />
+    <>   
+    <div className='static mt-[100px]'>
+      </div>       
+    <CardsLayout />
     </>
   )
 }
