39d814e0b92b1ca2d4ca7ac6d7c8ed8748b56068
sorinxv <sorinmartinescu06@gmail.com>
2022-12-14 19:15:18 +0100

    footer first update

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
   return <>
-  <div className="absolute bottom-0">
-    <p className="p-2">Copyright 2022, <a href="https://github.com/SorinXV/marketplace">Marketplace</a></p>
+  <div className=" fixed bottom-0 min-[500px]-absolute w-[100%] ">
+    <p className=" text-white bold g-background font-black text-md  pt-[5px] pl-2 pb-2 text-lg">Copyright 2022, 
+      <a href="https://github.com/SorinXV/marketplace" >Marketplace</a>
+    </p>
   </div>
   </>;
 }
