81e2c0e39d2e0ea9f8bad1b5a8a1f4f3e2414fab
sorinxv <sorinmartinescu06@gmail.com>
2022-12-14 19:26:02 +0100

    testing sizes

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
     <h1 className="m-2 text-white font-black h1-title-home ">Market
     <span className="font-black h1-title-home g-text">Place</span>
     </h1>
-    <h2 className="m-2 text-white font-black h1-title-home ">A  <span className="font-black g-text h1-title-home">Place</span> to buy and sell stuff.</h2>
+    <h2 className="m-2 text-white font-black h2-title">A  <span className="font-black g-text h2-title">Place</span> to buy and sell stuff.</h2>
     <Link href="/explore">
-    <button type="button" class="h2-title mt-[2vh] font-black text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-extrabold rounded-full px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ">Explore</button>
+    <button type="button" class="button mt-[2vh] font-black text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-extrabold rounded-full px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ">Explore</button>
     </Link> 
     </div>
     </center>
